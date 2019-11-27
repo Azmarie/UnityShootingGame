bd978b9e44b9c04e551b926bdae413f4174cf8e1
Azmarie Wang <azmarie.yx@gmail.com>
2019-11-27 12:58:26 -0800

    weapon switching

--- Assets/Scripts/WeaponSwitcher.cs
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -1,43 +1,45 @@
- using UnityEngine;
- using System.Collections;
- 
- public class WeaponSwitcher : MonoBehaviour {
- 
-     public GameObject[] weapons; // push your prefabs
- 
-     public int currentWeapon = 0;
-     
-     private int nrWeapons;
-     
-     void Start() {
-         
-         nrWeapons = weapons.Length;
-         
-         SwitchWeapon(currentWeapon); // Set default gun
-     
-     }
-     
-     void Update () {
-         for (int i=1; i <= nrWeapons; i++)    { 
-             if (Input.GetKeyDown("" + i)) {
-                 currentWeapon = i-1;
-                 
-                 SwitchWeapon(currentWeapon);
-             
-             }
-         }        
- 
-     }
-     
-     void SwitchWeapon(int index) {
- 
-         for (int i=0; i < nrWeapons; i++)    {
-             if (i == index) {
-                 weapons[i].gameObject.SetActive(true);
-             } else { 
-                 weapons[i].gameObject.SetActive(false);
-             }
-         }
-     }
-     
- }
+using UnityEngine;
+using System.Collections;
+
+// Inspiration from Youtube Tutorial: https://www.youtube.com/watch?v=Dn_BUIVdAPg&t=91s
+
+public class WeaponSwitcher: MonoBehaviour {
+
+	public GameObject[] weapons;
+	public int currentWeapon = 0;
+	private int nrWeapons = 2;
+
+	void Start() {
+		SwapGun(currentWeapon);
+	}
+
+	void Update() {
+        if (Input.GetKey(KeyCode.A)) {
+		    for (int i = 1; i <= nrWeapons; i++) {
+				currentWeapon = i - 1;
+				SwapGun(currentWeapon);
+                print(currentWeapon);
+
+			}
+		}
+
+        // if (Input.GetKey(KeyCode.A)) {
+        //     print(currentWeapon);
+        //     currentWeapon = 1-currentWeapon;
+		// }
+
+	}
+
+	void SwapGun(int index) {
+        print("SwapGun is happening");
+        // print(index);
+		for (int i = 0; i < nrWeapons; i++) {
+			if (i == index) {
+				weapons[i].gameObject.SetActive(true);
+			} else {
+				weapons[i].gameObject.SetActive(false);
+			}
+		}
+	}
+
+}
